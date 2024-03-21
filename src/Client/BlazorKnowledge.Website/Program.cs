fbcb5c0d932aa033c0752a6218b642a174caf82c
Marcin Hanzel <marcin.hanzel98@gmail.com>
2024-03-21 22:21:27 +0100

    Add Client and Server site Add config.props for files.

---
+++ src/Client/BlazorKnowledge.Website/Program.cs
@@ -0,0 +1,30 @@
+namespace BlazorKnowledge.Website;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddRazorPages();
+        builder.Services.AddServerSideBlazor();
+        
+
+        var app = builder.Build();
+
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.MapBlazorHub();
+        app.MapFallbackToPage("/_Host");
+
+        app.Run();
+    }
+}
